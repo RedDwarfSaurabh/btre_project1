79be65eba32f02b1ea7b45fc163bedcfd864fa6e
RedDwarfSaurabh <46939600+RedDwarfSaurabh@users.noreply.github.com>
2021-09-05 15:38:52 +0530

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb1',
         'USER': 'postgres',
-        'PASSWORD': '5643',
+        'PASSWORD': '*****',
         'HOST': 'localhost'
     }
 }
@@ -138,7 +138,7 @@ MESSAGE_TAGS = {
 
 # Email config
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='saurabhlypse1996@gmail.com'
+EMAIL_PORT = ********
+EMAIL_HOST_USER='****************'
 EMAIL_HOST_PASSWORD=''
 EMAIL_USE_TLS=True
